a2f2dd2daef6bb88b1ddcfb5bb5e51307e1879a7
SakshyamSB <np03cs4s240217@heraldcollege.edu.np>
2025-05-16 17:13:20 +0545

    My Information and Logout in header added

--- home/urls.py
+++ home/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('cast-vote/', views.cast_vote, name='cast_vote'),
     path('submit-vote/<int:election_id>/', views.submit_vote, name='submit_vote'),
     path('results/', views.election_results, name='election_results'),
-    # path('election-results/', views.election_results, name='election_results'),
-    # path('my-info/', views.my_info, name='my_info'),
-    # path('logout/', views.user_logout, name='logout')
+    path('election-results/', views.election_results, name='election_results'),
+    path('my-info/', views.my_info, name='my_info'),
+    path('logout/', views.user_logout, name='logout'),
 
 
     path('verify-email/<str:uidb64>/<str:token>/', views.verify_email, name='verify_email'),
--- home/views.py
+++ home/views.py
@@ -126,32 +126,6 @@ def open_home(request):
 
         
 
-# def open_registration(request):
-#     if request.method == 'POST':
-#         first_name = request.POST.get('first_name')
-#         last_name = request.POST.get('last_name')
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#         voter_id = request.POST.get('voter_id')  # Get the voter_id from the form
-
-#         # Check if user with this email already exists
-#         if CustomUser.objects.filter(email=email).exists():
-#             messages.error(request, "Email is already registered.")
-#         # Check if the provided voter_id already exists
-#         elif CustomUser.objects.filter(voter_id=voter_id).exists():
-#             messages.error(request, "This voter ID is already taken. Please use a different one.")
-#         else:
-#             # Create new user using CustomUser model
-#             user = CustomUser.objects.create_user(username=email, email=email, password=password, voter_id=voter_id)
-#             user.first_name = first_name
-#             user.last_name = last_name
-#             user.save()
-
-#             messages.success(request, "Registration successful! You can now login.")
-
-#     return render(request, 'home/registration.html')
-
-
 def cast_vote(request):
     elections = Election.objects.filter(start_date__lte=timezone.now(), end_date__gte=timezone.now())
 
@@ -197,6 +171,13 @@ def submit_vote(request, election_id):
 def election_results(request):
     return render(request, 'election_results.html')
 
-# @login_required
+@login_required
 def my_info(request):
-    return render(request, 'my_info.html') 
+    user = request.user  # Get the logged-in user
+    return render(request, 'my_info.html', {'user': request.user})
+
+
+def user_logout(request):
+    logout(request)  # Django's built-in logout function
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('login')  # Redirect to login page after logging out
